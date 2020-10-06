277e062482ca01e7cc289d29bd2165317cb83603
Lautaro <lautydb@yahoo.com>
2020-10-05 22:32:32 -0300

    Update chardev.c

--- sor2-tp0/chardev.c
+++ sor2-tp0/chardev.c
@@ -2,6 +2,7 @@
 #include <linux/kernel.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h> 
+#include <linux/ctype.h> 
 
 /*   
 *  Prototipos 
@@ -16,15 +17,15 @@ static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 #define SUCCESS 0
 #define DEVICE_NAME "chardev"   /* Nombre del dispositivo que va a aparecer en /proc/devices */
 #define BUF_LEN 80              /* Maxima longitud del mensaje ingresado para el dispositivo */
-
+#define CONST_N 1               /* Valor que utilizo para la encriptacion con cesar */
 /*  
 * Variables globales
 */
 static int Major;               /* Major number asignado al driver */
 static int Device_Open = 0;     /* Lo usamos para prevenir que se habran multiples accesos al dispositivo */
 static char msg[BUF_LEN];       /* El mensaje que el dispositivo va a devolver cuando sea consultado */
-static char *msg_Ptr;           /* Memoria del buffer */
-static struct file_operations fops = {      
+static char *memoria_buffer;           /* Memoria del buffer */
+static struct file_operations fops = {  
   .read = device_read,        
   .write = device_write,        
   .open = device_open,        
@@ -40,6 +41,14 @@ int init_module(void)
     return Major;        
   }
   printk(KERN_INFO "Driver chardev registrado");
+  printk(KERN_INFO "Tengo major number %d. Hablarle al driver ", Major);
+  printk(KERN_INFO ", crear un dev_file con \n");
+  printk(KERN_INFO "sudo rm /dev/%s\n", DEVICE_NAME);
+  printk(KERN_INFO "sudo mknod /dev/%s c %d 0\n", DEVICE_NAME, Major);
+  printk(KERN_INFO "sudo chmod 666 /dev/%s\n", DEVICE_NAME);
+  printk(KERN_INFO "Probar varios minor numbers. Probar cat y echo\n");
+  printk(KERN_INFO "Al device file.\n");
+  printk(KERN_INFO "Eliminar el /dev y el modulo al terminar.\n");
   
   return 0;
 }
@@ -53,13 +62,16 @@ void cleanup_module(void)
 * LLamado cuando un usuario intenta abrir el dispositivo, "cat /dev/mycharfile" 
 */
 static int device_open(struct inode *inode, struct file *file)
-{              
-  static int counter = 0;        
+{                    
+  #ifdef DEBUG
+  printk(KERN_INFO "device_open(%p)\n", file);
+  #endif
+
   if (Device_Open)                
     return -EBUSY;        
-  Device_Open++;             
-  msg_Ptr = msg;        
-  try_module_get(THIS_MODULE);      
+  Device_Open++;            
+
+  memoria_buffer = msg;     
   return SUCCESS;
 }
 /*  
@@ -67,8 +79,7 @@ static int device_open(struct inode *inode, struct file *file)
 */
 static int device_release(struct inode *inode, struct file *file)
 {          
-  Device_Open--;          /* Desminuimos el valor, para determinar que esta listo para el proximo llamado */        
-  module_put(THIS_MODULE);         
+  Device_Open--;          /* Desminuimos el valor, para determinar que esta listo para el proximo llamado */      
   return 0;
 }
 /*  
@@ -76,36 +87,37 @@ static int device_release(struct inode *inode, struct file *file)
 */
 static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_t * offset)
 {
-  printk(KERN_ALERT "EMPEZO A LEER\n");        
   int bytes_read = 0;    // Numero de bytes escritos en el buffer
-  if (*msg_Ptr == 0)     //si estamos en el final del mensaje retornamos 0           
+  if (*memoria_buffer == 0)     //si estamos en el final del mensaje retornamos 0           
     return 0;        
   
-  while (length && *msg_Ptr) {        //ponemos la informacion en el buffer
-    put_user(*(msg_Ptr++), buffer++);             
+  while (length && *memoria_buffer) {        //ponemos la informacion en el buffer
+    put_user(*(memoria_buffer++), buffer++);             
     length--;                
     bytes_read++;        
-  }
- // copy_to_user(to,from,bytes_read);      //mando mensajes al usuario (output)   
-  printk(KERN_ALERT "Dispositivo mostrando mensaje encriptado:\n");
+    // copy_to_user(to,from,bytes_read);      //mando mensajes al usuario (output) 
+  }  
+  printk(KERN_INFO "Dispositivo mostrando mensaje encriptado:\n");
   return bytes_read;  //La mayoria de las funciones de lectura, retornan la cantindad de bytes puestos en el buffer
  }
 
 /*   
 * Llamado cuando un usuario escribe al chardev: echo "hi" > /dev/chardev
 */
-static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
+static ssize_t device_write(struct file *filp, const char *buff, size_t length, loff_t * off)
 {        
   int i;
  // copy_from_user(to,from,bytes);  //traigo lo que puso el usuario (input)
-  for (i = 0; i < len && i < BUF_LEN; i++)
-    get_user(msg[i], buff + i);
+  for (i = 0; i < length && i < BUF_LEN; i++){
+    get_user(msg[i], buff + i);       //Guardo el mensaje en un array asi puedo operar en el
+   // msg[i] = cesar(msg[i], CONST_N);  //Aplico cesar y modifico la posición en el array
+  }
 
-  msg[len]= '\0';
-  msg_Ptr = msg;
-  printk(KERN_ALERT "Dispositivo encriptando:\n");    
+  msg[length]= '\0';
+  memoria_buffer = msg;
+  printk(KERN_INFO "Dispositivo encripto el mensaje:\n");    
 
-  return -EINVAL;
+  return length;
 }
 
 MODULE_LICENSE("GPL");
